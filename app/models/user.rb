dce85ffb4acfb51a615d638a66c2e2149f8db030
kelseylandau <klandau@chicagobooth.edu>
2020-03-20 02:23:31 +0000

    accepted FR

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,18 @@ class User < ApplicationRecord
   validates :email, :presence => true
   has_secure_password
 
-  has_many :country_visits, :dependent => :destroy
-  has_many :received_follow_requests, :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy
-  has_many :sent_follow_requests, :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy
   has_many :country_hit_lists, :dependent => :destroy
+  has_many :country_visits, :dependent => :destroy
+
+  has_many :received_follow_requests, :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy
+    has_many(:accepted_recieved_follow_requests, -> { where({ :status => "accepted" }) }, { :class_name => "FollowRequest", :foreign_key => "recipient_id" })
+    has_many(:pending_recieved_follow_requests, -> { where({ :status => "pending" }) }, { :class_name => "FollowRequest", :foreign_key => "recipient_id" })
+    has_many(:declined_recieved_follow_requests, -> { where({ :status => "declined" }) }, { :class_name => "FollowRequest", :foreign_key => "recipient_id" })
+
+  has_many :sent_follow_requests, :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy
+    has_many(:accepted_sent_follow_requests, -> { where({ :status => "accepted" }) }, { :class_name => "FollowRequest", :foreign_key => "sender_id" })
 
   has_many :leaders, :through => :sent_follow_requests, :source => :recipient
   has_many :followers, :through => :received_follow_requests, :source => :sender
+    has_many :pending_followers, :through => :pending_recieved_follow_requests, :source => :sender
 end
